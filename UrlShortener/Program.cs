037945916d91e9bf387f6fc1544e679959cca1bd
Ionut Radu <radutudorion@gmail.com>
2024-02-17 23:42:26 +0200

    Add project files.

---
+++ UrlShortener/Program.cs
@@ -0,0 +1,74 @@
+
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Entities;
+using UrlShortener.Models;
+using UrlShortener.Services;
+
+namespace UrlShortener;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+        });
+
+        builder.Services.AddScoped<UrlShorteningService>();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.MapPost("api/shorten", async (
+                ShortenUrlRequest request,
+                UrlShorteningService urlShorteningService,
+                ApplicationDbContext dbContext,
+                HttpContext httpContext) =>
+        {
+            if (!Uri.TryCreate(request.Url, UriKind.Absolute, out var uri))
+            {
+                return Results.BadRequest("Invalid URL");
+            }
+
+            var code = await urlShorteningService.GenerateUniqueCode();
+            var shortenedUrl = new ShortenedUrl
+            {
+                Id = Guid.NewGuid(),
+                LongUrl = request.Url,
+                Code = code,
+                ShortUrl = $"{httpContext.Request.Scheme}://{httpContext.Request.Host}/{code}",
+                CreatedAtUtc = DateTime.UtcNow
+            };
+
+            dbContext.ShortenedUrls.Add(shortenedUrl);
+            await dbContext.SaveChangesAsync();
+
+            return Results.Created(shortenedUrl.ShortUrl, shortenedUrl);
+        });
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
---
+++ UrlShortener/Services/UrlShorteningService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UrlShortener.Services;
+
+public class UrlShorteningService
+{
+    public const int NumberOfCharsInShortLink = 7;
+    private const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    private readonly Random random = new Random();
+    private readonly ApplicationDbContext dbContext;
+
+    public UrlShorteningService(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<string> GenerateUniqueCode()
+    {
+        var codechars = new char[NumberOfCharsInShortLink];
+
+        while (true)
+        {
+            for (var i = 0; i < NumberOfCharsInShortLink; i++)
+            {
+                var randomIndex = random.Next(Alphabet.Length - 1);
+                codechars[i] = Alphabet[randomIndex];
+            }
+
+            var code = new string(codechars);
+
+            if (! (await dbContext.ShortenedUrls.AnyAsync(s => s.Code == code)))
+            {
+                return code;
+            }
+        }
+    }
+}
